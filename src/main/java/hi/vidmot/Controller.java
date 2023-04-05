342c29753a625d314a536115c22b410f27e4608e
Maria Ros <mrk9@hi.is>
2023-04-05 14:43:28 +0000

    setBackroundPositions method bætt við

--- src/main/java/hi/vidmot/Controller.java
+++ src/main/java/hi/vidmot/Controller.java
@@ -2,8 +2,21 @@ package hi.vidmot;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.Region;
 
 public class Controller {
+
+    private void setBackgroundPositions(Region region, double xPosition) {
+        String style = "-fx-background-position: " +
+                "left " + xPosition/6 + "px bottom," +
+                "left " + xPosition/5 + "px bottom," +
+                "left " + xPosition/4 + "px bottom," +
+                "left " + xPosition/3 + "px bottom," +
+                "left " + xPosition/2 + "px bottom," +
+                "left " + xPosition + "px bottom;";
+        region.setStyle(style);
+    }
+
     @FXML
     public void onExitButton(ActionEvent actionEvent) {
         System.exit(2);
