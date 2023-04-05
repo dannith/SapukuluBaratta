59523305685f71694547b31a939b40e76b901532
aph8 <andri1998@gmail.com>
2023-04-05 14:34:19 +0000

    breytt nafn

--- src/main/java/hi/vidmot/application.java
+++ src/main/java/hi/vidmot/application.java
@@ -1,16 +1,15 @@
 package hi.vidmot;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(application.class.getResource("forsida-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Sápukúlubarátta");
         stage.setScene(scene);
--- src/main/java/hi/vidmot/controller.java
+++ src/main/java/hi/vidmot/controller.java
@@ -2,9 +2,8 @@ package hi.vidmot;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
-public class HelloController {
+public class controller {
     @FXML
     public void onExitButton(ActionEvent actionEvent) {
         System.exit(2);
