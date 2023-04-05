68f456e9c3ddcb7f21bbe23652d690f06c24aa19
Maria Ros <mrk9@hi.is>
2023-04-05 14:46:09 +0000

    initialize aðferð bætt við+fx:id á StackPane+Region breyta content í Controller

--- src/main/java/hi/vidmot/Controller.java
+++ src/main/java/hi/vidmot/Controller.java
@@ -1,10 +1,31 @@
 package hi.vidmot;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 public class Controller {
+
+
+    public Region content;
+
+    public void initialize()
+    {
+        DoubleProperty xPosition = new SimpleDoubleProperty(0);
+        xPosition.addListener((observable, oldValue, newValue) -> setBackgroundPositions(content, xPosition.get()));
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(xPosition, 0)),
+                new KeyFrame(Duration.seconds(200), new KeyValue(xPosition, -15000))
+        );
+        timeline.play();
+    }
 
     private void setBackgroundPositions(Region region, double xPosition) {
         String style = "-fx-background-position: " +
