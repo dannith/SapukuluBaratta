474010a39f0167b3d757c7fb9da7f45243673cb8
aph8 <91262471+aph8@users.noreply.github.com>
2023-04-06 16:07:09 +0000

    prufa

--- src/main/java/hi/vinnsla/Hljod.java
+++ src/main/java/hi/vinnsla/Hljod.java
@@ -3,14 +3,15 @@ package hi.vinnsla;
 import java.io.File;
 import javafx.scene.;
 
+
 public class Hljod {
     public final String forsidatonlist = "C:\\Users\\Lenovo\\IdeaProjects\\lokaverk\\SapukuluBaratta\\src\\main\\resources\\hi\\Tonlist\\Forsida.mp3";
     public final String leikjatonlist = "";
 
     private File file;
-    private Media media;
+    //private Media media;
 
-    private MediaPlayer mediaPlayer;
+    //private MediaPlayer mediaPlayer;
 
     //kalli
 
