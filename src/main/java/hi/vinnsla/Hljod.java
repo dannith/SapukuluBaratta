294d51e456c15911698167feb517ad56ac9dd5f5
aph8 <andri1998@gmail.com>
2023-04-12 12:45:15 +0000

    bara pæla æla

--- src/main/java/hi/vinnsla/Hljod.java
+++ src/main/java/hi/vinnsla/Hljod.java
@@ -5,17 +5,14 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 
-
 public class Hljod {
     public static final String forsidatonlist = "C:\\Users\\andri\\IdeaProjects\\SapukuluBaratta\\src\\main\\resources\\hi\\Tonlist\\Forsida.mp3";
-    //public final String leikjatonlist = "";
-    private static File file;
-    private static Media media;
     private static MediaPlayer mediaPlayer;
 
     public static void play(){
-        file = new File(forsidatonlist);
-        media = new Media(file.toURI().toString());
+        //public final String leikjatonlist = "";
+        File file = new File(forsidatonlist);
+        Media media = new Media(file.toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayer.play();
