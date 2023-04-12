6f592e3dcba9b82dd3d1a08bbe7d756c6315a462
Maria Ros <mrk9@hi.is>
2023-04-12 14:18:44 +0000

    setIcon aðferð bætt í Application

--- src/main/java/hi/vidmot/MainApplication.java
+++ src/main/java/hi/vidmot/MainApplication.java
@@ -4,16 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApplication extends Application {
+private Image setIcon(){
+    return new Image("bubble.png");
+
+}
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("forsida-view.fxml"));
         stage.setTitle("Bubble Struggle");
         Scene scene  = new Scene(root);
+
         stage.setScene(scene);
+        stage.getIcons().add(setIcon());
         stage.show();
     }
 
