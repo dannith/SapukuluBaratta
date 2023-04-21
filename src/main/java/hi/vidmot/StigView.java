d3a09bc9525d77a2acc1e1256b065f6407698dc6
Maria Ros <mrk9@hi.is>
2023-04-21 23:35:12 +0000

    Stigatafla

--- src/main/java/hi/vidmot/StigView.java
+++ src/main/java/hi/vidmot/StigView.java
@@ -6,12 +6,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StigView extends Pane {
 
     @FXML
     private ListView<Integer> fxScoreBoard;
+    /**
+     * Listi sem heldur utan um stig leikmanns í hverri tilraun
+     */
+    private static List<Integer> score = new ArrayList<>();
 
+    /**
+     * Aðferð sem bætir við stigum leikmanns í listann
+     * @param stig
+     */
+    public static void addScore(int stig){
+        score.add(stig);
+    }
+
+    /**
+     * Aðferð sem loadar sérhæfða klasanum score-view ásamt því að uppfæra stigatöflu
+     */
     public StigView(){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("score-view.fxml"));
         fxmlLoader.setRoot(this);
@@ -21,8 +38,8 @@ public class StigView extends Pane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-
-        //fxScoreBoard.getItems().add(GameManager.getScore());
+        addScore(GameManager.score.getValue());
+        fxScoreBoard.getItems().addAll(score);
 
     }
 }
